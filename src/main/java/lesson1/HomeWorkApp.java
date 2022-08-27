d55fad85753ce7ca59258f377a253b884976ceda
artem240992 <artem240992@bk.ru>
2022-08-27 16:51:45 +0300

    Update HomeWorkApp.java

--- src/main/java/lesson1/HomeWorkApp.java
+++ src/main/java/lesson1/HomeWorkApp.java
@@ -14,7 +14,7 @@ public class HomeWorkApp {
 
 
     static void printThreeWords() {
-        System.out.println("Orange");
+        System.out.println("Orange ");
         System.out.println("Banana");
         System.out.println("Apple");
     }
@@ -54,7 +54,7 @@ else
         if (a>=b){
             System.out.println("a >= b");
         }
-        else System.out.println("a < bb");
+        else System.out.println("a < b");
 
     }
 }
